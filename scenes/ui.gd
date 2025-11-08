5634dc56d6f291b45b7434e2a9dc5a4f162409e6
VorahPong <vorahpongmean@gmail.com>
2025-11-08 00:47:21 -0600

    Updated health bar (now function)

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,8 +1,18 @@
+# ui.gd
+
 extends CanvasLayer  # (or Node, if UI is a plain Node)
 
 @onready var coins_label: Label = %CoinsLabel
+@onready var hearts_container: HBoxContainer = %HBoxContainer
 
 func _ready() -> void:
 	# Register this label with the autoload GameManager
+	
+	# COINS LABEL
 	GameManager.register_label(coins_label)
 	GameManager.on_level_entered()
+	#
+	
+	# HEALTH
+	HealthManager.register_hearts(hearts_container)
+	#
--- scripts/HealthManager.gd
+++ scripts/HealthManager.gd
@@ -1,13 +1,30 @@
+# HealthManager.gd
+
 extends Node
 
-var max_health = 3
-var current_health = max_health
+var max_health := 3
+var current_health := max_health
+var hearts_node: Node = null
 
-# Health UI Update function (you can call this from other scripts)
-func update_health_ui(hearts_node: Node):
+func register_hearts(node: Node):
+	hearts_node = node
+	print("[HM] register_hearts -> ", hearts_node)
+	update_health_ui()
+
+func update_health_ui():
+	if hearts_node == null:
+		print("[HM] update_health_ui skipped: hearts_node is NULL")
+		return
+	print("[HM] update_health_ui current_health=", current_health,
+		  " children=", hearts_node.get_child_count())
 	for i in range(hearts_node.get_child_count()):
 		var heart = hearts_node.get_child(i)
-		if i < current_health:
-			heart.visible = true
-		else:
-			heart.visible = false
+		heart.visible = (i < current_health)
+		# helpful:
+		# print("[HM] heart ", heart.name, " visible=", heart.visible)
+
+func lose_one_heart():
+	if current_health > 0:
+		current_health -= 1
+	print("[HM] lose_one_heart -> current_health=", current_health)
+	update_health_ui()

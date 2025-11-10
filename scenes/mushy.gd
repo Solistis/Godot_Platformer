0c490facc64e84d80e109a4c511b0a5ead644e24
VorahPong <vorahpongmean@gmail.com>
2025-11-10 15:18:21 -0600

    Updated mushy behavior

--- scenes/mushy.gd
+++ scenes/mushy.gd
@@ -4,7 +4,6 @@ extends RigidBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-
 #func _on_area_2d_body_entered(body):
 	#if (body.name == "CharacterBody2D"):
 		#
@@ -20,20 +19,20 @@ extends RigidBody2D
 			#body.jump() # make character bounce
 			#
 
-func _on_damage_hitbox_body_entered(body): print("DMG enter:", body)
 
-#func _on_damage_hitbox_body_entered(body: Node2D) -> void:
-	##pass
-	#if body.name == "CharacterBody2D":
-		#print("Player touched DamageHitbox!")
-		#body.jump()
-		#HealthManager.lose_one_heart()
+# When player touch the top of the mushy
+func _on_kill_mushy_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		print("Hit from above")
+		animated_sprite.play("hit")
+		queue_free()
+		body.jump() # make character bounce
 
 
-#func _on_kill_mushy_body_entered(body: Node2D) -> void:
-	##pass # Replace with function body.
-	#if body.name == "CharacterBody2D":
-		#print("Hit from above")
+# When player touch the side of the mushy
+func _on_damage_hitbox_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		print("Hit from the side")
 		#animated_sprite.play("hit")
-		#queue_free()
-		#body.jump() # make character bounce
+		HealthManager.lose_one_heart()
+		(body as CharacterBody2D).apply_knockback(global_position)  # or call_deferred(...)

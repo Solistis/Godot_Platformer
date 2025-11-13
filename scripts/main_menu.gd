ec81d2070e80651b9213a023698722e7e8a2e889
solistis <solistis01@gmail.com>
2025-11-13 15:20:24 -0600

    added fall background in Town. Issue: mc now falls endlessly

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,4 +2,4 @@ extends Node
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/BeforeTown.tscn")
+	get_tree().change_scene_to_file("res://scenes/Town.tscn")		#fix this back to BeforeTown before pull request is made
